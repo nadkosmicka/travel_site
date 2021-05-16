529ad315446014ca23a1d2057676bf732db43b9d
nadkosmicka <44292852+nadkosmicka@users.noreply.github.com>
2021-05-16 16:09:33 +0200

    Add files via upload

---
+++ travel_site/index.php
@@ -0,0 +1,63 @@
+<?php include "header.php"; ?>
+
+<!-- sky -->
+
+<section class="sky">
+     <aside>
+         <ul>
+             <li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+             <li><a href=""><i class="fab fa-youtube-square"></i></a></li>
+             <li><a href=""><i class="fab fa-instagram"></i></a></li>
+             <li><a href=""><i class="fab fa-pinterest"></i></a></li>
+         </ul>
+ </aside>
+ <div class="container">
+     <article>
+             <h1>Vase putovanje iz snova</h1>
+             <p>Izaberite svoju avanturu</p>
+             <button>Pretrazite</button>
+     </article>
+ </div>
+</section>
+ 
+<!-- main -->
+<main class="destination container">
+     <section class="title">
+         <h2>Najpopularnije destinacije</h2>
+         <p>Rezervišite do kraja meseca za 30% popusta</p>
+     </section>
+     <section>
+         <div class="wrapper">
+             <img src="img/grcka.jpg" alt="grcka">
+             <h6><i class="fas fa-map-marker-alt"></i>Grcka</h6>
+         </div>
+
+         <div class="wrapper">
+             <img src="img/italija.jpg" alt="italija">
+             <h6><i class="fas fa-map-marker-alt"></i>Italija</h6>
+         </div>
+
+         <div class="wrapper">
+             <img src="img/kanada.jpg" alt="kanada">
+             <h6><i class="fas fa-map-marker-alt"></i>Kanada</h6>
+         </div>
+
+         <div class="wrapper">
+             <img src="img/norveska.jpg" alt="norveska">
+             <h6><i class="fas fa-map-marker-alt"></i>Norveska</h6>
+         </div>
+
+         <div class="wrapper">
+             <img src="img/rusija.jpg" alt="rusija">
+             <h6><i class="fas fa-map-marker-alt"></i>Rusija</h6>
+         </div>
+
+         <div class="wrapper">
+             <img src="img/spanija.jpg" alt="spanija">
+             <h6><i class="fas fa-map-marker-alt"></i>Spanija</h6>
+         </div>
+
+     </section>
+ </main>
+
+ <?php include "footer.php"; ?>
---
+++ travel_site/ponuda.php
@@ -0,0 +1,56 @@
+<?php include "header.php"; ?>
+    
+    <!-- aifel -->
+
+    <section class="aifel">
+        <aside>
+            <ul>
+                <li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href=""><i class="fab fa-youtube-square"></i></a></li>
+                <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                <li><a href=""><i class="fab fa-pinterest"></i></a></li>
+            </ul>
+    </aside>
+    <div class="container">
+        <article>
+                <h1>Popust 10-30%</h1>
+                <p>Uplata do januara 2021.</p>
+                <button>Pretrazite</button>
+        </article>
+    </div>
+   </section>
+
+   <!-- main ponuda -->
+
+   <main class="ponuda container">
+        <section class="title">
+            <h2>Iskoristite promotivne pakete</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem qui impedit accusantium reprehenderit
+                inventore ut. Dolore, sint! Totam, dolorum ab.</p>
+        </section>
+
+        <section>
+            <article>
+                <img src="img/ponuda1.jpg" alt="grcka">
+            </article>
+            <article>
+                <div>
+                    <img src="img/ponuda2.jpg" alt="">
+                </div>
+                <div>
+                    <h3>Najbolje cene</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat asperiores molestias ad? Eligendi ad asperiores adipisci rerum iste officiis id.</p>
+                </div>
+                <div>
+                    <h3>Najbolje lokacije</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat asperiores molestias ad? Eligendi ad asperiores adipisci rerum iste officiis id.</p>
+                </div>
+                <div>
+                    <img src="img/ponuda3.jpg" alt="">
+                </div>
+            </article>
+
+        </section>
+</main>
+    
+<?php include "footer.php"; ?>
